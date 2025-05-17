aa21da80c6133ebca4cab0d5af3043a535f93ebf
Monica-Holberton <monicafarid7@gmail.com>
2025-05-17 08:56:12 +0000

    Bubble Sort!

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,39 @@
+#include "sort.h"
+#include <stdio.h>
 
+/**
+* bubble_sort - Sorts an array of integers in ascending order
+*               using the Bubble sort algorithm.
+* @array: Sorted Array
+* @size: Size of the Array
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+size_t i, j;
+int temp;
+int swap;
+
+for (i = 0; i < size - 1; i++)
+{
+swap = 0;
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+/* Swap the elements */
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+
+/* Print the array after each swap */
+print_array(array, size);
+swap = 1;
+}
+}
+
+/* If no two elements were swapped by inner loop, break */
+if (swap == 0)
+break;
+}
+}
---
+++ print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers.
+ * @array: Array to be printed
+ * @size: Size of the array
+ */
+void print_array(int *array, size_t size)
+{
+size_t i;
+for (i = 0; i < size; i++)
+{
+printf("%d", array[i]);
+if (i < size - 1)
+printf(", ");
+}
+printf("\n");
+}
